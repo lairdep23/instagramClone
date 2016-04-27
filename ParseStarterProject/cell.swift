8b7a514d90bf3bd0b0539c9cb5eedc44bcc93230
Evan <Evan@Evans-MacBook-Pro-2.local>
2016-04-26 22:38:32 -0500

    finished app

---
+++ ParseStarterProject/cell.swift
@@ -0,0 +1,18 @@
+//
+//  cell.swift
+//  InstagramClone
+//
+//  Created by Evan on 4/26/16.
+//  Copyright © 2016 Parse. All rights reserved.
+//
+
+import UIKit
+
+class cell: UITableViewCell {
+
+    @IBOutlet weak var postedImage: UIImageView!
+    
+    @IBOutlet weak var username: UILabel!
+
+    @IBOutlet weak var message: UILabel!
+}
